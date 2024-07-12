0e4ab262b03346f3ea7c46fa57325da44ee83814
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-07-12 14:26:22 +0600

    added product section radios

--- src/pages/addProduct/Added.tsx
+++ src/pages/addProduct/Added.tsx
@@ -1,126 +1,180 @@
-import { useState } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ChangeEvent, useState, FormEvent } from "react";
 import { Rating, ThinStar } from "@smastrom/react-rating";
-
 import "@smastrom/react-rating/style.css";
+import axios from "axios";
+
+interface TFormData {
+  title: string;
+  price: string;
+  image: string; // Changed from File | null to string to store image URL
+  rating: number;
+  selectedValue: string;
+  message: string;
+}
 
 const Added = () => {
-  const [selectedValue, setSelectedValue] = useState("All");
-  const [rating, setRating] = useState(0);
+  const IMG_IMG_HOSTING = `https://api.imgbb.com/1/upload?key=${
+    import.meta.env.VITE_API_KEY_IMGBB
+  }`;
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    event.preventDefault();
-    setSelectedValue(event.target.value);
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const [rating, setRating] = useState(0);
+  const [selectedValue, setSelectedValue] = useState("All");
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData.append("image", file);
+
+        const res = await axios.post(IMG_IMG_HOSTING, formData, {
+          headers: { "Content-Type": "multipart/form-data" },
+        });
+
+        const image = res.data.data.url;
+
+        const submittedData: TFormData = {
+          title,
+          price,
+          image,
+          rating,
+          selectedValue,
+          message,
+        };
+
+        console.log("Form Data Submitted:", submittedData);
+        // Handle form submission (e.g., send data to an API)
+
+        // Reset form fields
+        setTitle("");
+        setPrice("");
+        setFile(null);
+        setRating(0);
+        setSelectedValue("All");
+        setMessage("");
+      } catch (error) {
+        console.error("Error uploading image:", error);
+        // Handle the error accordingly
+      }
+    }
   };
 
   return (
-    <div>
-      <form className="max-w-xl mx-auto my-10">
+    <form className="max-w-xl mx-auto my-10" onSubmit={handleSubmit}>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="text"
+          name="Title"
+          id="Title"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label
+          htmlFor="Title"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-0 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Product Title
+        </label>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-6">
         <div className="relative z-0 w-full mb-5 group">
           <input
-            type="text"
-            name="Title"
-            id="Title"
+            type="number"
+            name="price"
+            id="price"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
           />
           <label
-            htmlFor="Title"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-0 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            htmlFor="price"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-0 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Product Title
+            Price
           </label>
         </div>
-        <div className="grid md:grid-cols-2 md:gap-6">
-          <div className="relative z-0 w-full mb-5 group">
-            <input
-              type="number"
-              name="price"
-              id="price"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
-            />
-            <label
-              htmlFor="fprice"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-0 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Price
-            </label>
-          </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <label
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              htmlFor="user_avatar"
-            >
-              Upload file
-            </label>
-            <input
-              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-              aria-describedby="user_avatar_help"
-              id="user_avatar"
-              type="file"
-            />
-          </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            aria-describedby="user_avatar_help"
+            id="user_avatar"
+            type="file"
+            onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
+          />
         </div>
-        <div className="grid md:grid-cols-2 md:gap-6">
-          <div className="relative z-0 w-full mb-5 group">
-            <Rating
-              style={{ maxWidth: 200 }}
-              value={rating}
-              onChange={setRating}
-              itemStyles={{
-                itemShapes: ThinStar,
-                activeFillColor: "#ffb700",
-                inactiveFillColor: "#fbf1a9",
-              }}
-            />
-          </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <select
-              id="pricingType"
-              name="pricingType"
-              className="w-full h-10 border-2 border-sky-500 focus:outline-none focus:border-sky-500 text-sky-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider"
-              value={selectedValue}
-              onChange={handleChange}
-            >
-              <option value="All" selected={selectedValue === "All"}>
-                All
-              </option>
-              <option value="Freemium" selected={selectedValue === "Freemium"}>
-                Freemium
-              </option>
-              <option value="Free" selected={selectedValue === "Free"}>
-                Free
-              </option>
-              <option value="Paid" selected={selectedValue === "Paid"}>
-                Paid
-              </option>
-            </select>
-          </div>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 w-full mb-5 group">
+          <Rating
+            style={{ maxWidth: 200 }}
+            value={rating}
+            onChange={setRating}
+            itemStyles={{
+              itemShapes: ThinStar,
+              activeFillColor: "#ffb700",
+              inactiveFillColor: "#fbf1a9",
+            }}
+          />
         </div>
-        <div>
-          <label
-            htmlFor="message"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        <div className="relative z-0 w-full mb-5 group">
+          <select
+            id="pricingType"
+            name="pricingType"
+            className="w-full h-10 border-2 text-gray-900  border-gray-300  cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider"
+            value={selectedValue}
+            onChange={handleChange}
           >
-            Your message
-          </label>
-          <textarea
-            id="message"
-            rows={4}
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Leave a comment..."
-          ></textarea>
+            <option value="Flower Plants">Flower Plants</option>
+            <option value="Fruits Plants">Fruits Plants</option>
+            <option value="Bonsai Plants">Bonsai Plants</option>
+            <option value="Bamboo Plants">Bamboo Plants</option>
+            <option value="Cactus Plants">Cactus Plants</option>
+            <option value="Rose Plants">Rose Plants</option>
+            <option value="Plants Pot">Bamboo Plants</option>
+            <option value="Fertilizers">Fertilizers</option>
+            <option value="Mango Plants">Mango Plants</option>
+            <option value="Gardening Tools">Gardening Tools</option>
+            <option value="Pebbles">Pebbles</option>
+          </select>
         </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      </div>
+      <div>
+        <label
+          htmlFor="message"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-          Submit
-        </button>
-      </form>
-    </div>
+          Your message
+        </label>
+        <textarea
+          id="message"
+          rows={4}
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Leave a comment..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-2xl text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-400 dark:hover:bg-blue-400 dark:focus:ring-blue-500 mt-2 md:mt-2 xl:mt-5"
+      >
+        Submit
+      </button>
+    </form>
   );
 };
 
