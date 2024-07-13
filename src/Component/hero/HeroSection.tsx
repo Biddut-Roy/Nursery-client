26f789bb858db91c9c18f87db25a86350c2dfd39
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-07-13 15:17:19 +0600

    hero picture change

--- src/Component/hero/HeroSection.tsx
+++ src/Component/hero/HeroSection.tsx
@@ -4,25 +4,25 @@ const HeroSection = () => {
       <div className="relative bg-gradient-to-r from-purple-600 to-blue-600 h-screen text-white overflow-hidden">
         <div className="absolute inset-0">
           <img
-            src="https://nurseryplantsbd.com/wp-content/uploads/2022/02/at-home.webp"
+            src="https://res.cloudinary.com/dbv2zv2ek/image/upload/v1720861630/SPR-houseplants-grown-in-water-_n9t1nk.jpg"
             alt="Background Image"
             className="bg-cover object-center min-h-screen "
           />
-          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute inset-0 bg-black opacity-10"></div>
         </div>
 
         <div className="relative z-10 flex flex-col justify-center items-center h-svh text-center">
           <h1 className="text-5xl font-bold leading-tight mb-4">
             Welcome to Our Awesome Website
           </h1>
-          <p className="text-lg text-gray-300 mb-8">
+          <p className="text-lg text-white mb-8">
             Discover amazing features and services that await you.
           </p>
           <a
             href="#"
-            className="bg-yellow-400 text-gray-900 hover:bg-yellow-300 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
+            className="bg-blue-500 text-gray-900 hover:bg-blue-300 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
           >
-            Get Started
+            Scroll down
           </a>
         </div>
       </div>
