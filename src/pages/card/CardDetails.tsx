935f7aa4d00f8ad9a467d93e5fc73343debe7587
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-07-15 12:45:50 +0600

    product added sonnar is successfully

--- src/pages/card/CardDetails.tsx
+++ src/pages/card/CardDetails.tsx
@@ -5,6 +5,7 @@ import { TProduct } from "../Management/ProductAndCategoryM";
 import { useAppDispatch } from "../../redux/hooks";
 import { addProduct } from "../../redux/features/auth/authSlice";
 import { Rating } from "@smastrom/react-rating";
+import { toast, Toaster } from "sonner";
 
 const CardDetails = () => {
   const { id } = useParams();
@@ -13,61 +14,66 @@ const CardDetails = () => {
   if (isLoading) {
     return <p>Loading....</p>;
   }
-  console.log(data);
 
   const handleCardDetails = (product: TProduct) => {
-    dispatch(addProduct(product));
+    const success = dispatch(addProduct(product));
+    if (success) {
+      toast.success("Product added Successfully");
+    }
   };
 
   return (
-    <div className="antialiased bg-gray-200 font-sans">
-      <div className="flex items-center justify-center min-h-screen ">
-        <div className="max-w-md md:max-w-2xl px-2 ">
-          <div className="bg-white shadow-xl overflow-hidden md:flex rounded-xl">
-            <div className="bg-cover bg-bottom h-56 md:h-auto md:w-56 pt-5">
-              <img src={data?.data.image} alt={data?.data.title} />
-            </div>
-            <div>
-              <div className="p-4 md:p-5">
-                <p className="font-bold text-xl md:text-2xl">
-                  {data?.data.title}
-                </p>
-                <p className="text-gray-700 md:text-lg">
-                  {data?.data.description}
-                </p>
+    <div>
+      <Toaster position="top-right" />
+      <div className="antialiased bg-gray-200 font-sans">
+        <div className="flex items-center justify-center min-h-screen ">
+          <div className="max-w-md md:max-w-2xl px-2 ">
+            <div className="bg-white shadow-xl overflow-hidden md:flex rounded-xl">
+              <div className="bg-cover bg-bottom h-56 md:h-auto md:w-56 pt-5">
+                <img src={data?.data.image} alt={data?.data.title} />
               </div>
-              <div className="p-4 md:p-5 bg-gray-100 rounded-tl-xl">
-                <div className="sm:flex sm:justify-between sm:items-center ">
-                  <div>
-                    <div className="text-lg text-gray-700">
-                      <span className="text-gray-900 font-bold">
-                        {data?.data.quantity}
-                      </span>{" "}
-                      Product
-                    </div>
-
-                    <div className="flex -mx-px">
-                      <Rating
-                        style={{ maxWidth: 100 }}
-                        value={data?.data.rating}
-                        readOnly
-                      />
-                    </div>
-                  </div>
-                  <div className="text-lg text-gray-700">
-                    <span className="text-gray-900 font-bold">Price:</span>{" "}
-                    {data?.data.price}
-                  </div>
-                  <button
-                    className="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 bg-blue-400 hover:bg-blue-500 font-bold text-white md:text-lg rounded-2xl shadow-md"
-                    onClick={() => handleCardDetails(data?.data)}
-                  >
-                    Add to Card
-                  </button>
+              <div>
+                <div className="p-4 md:p-5">
+                  <p className="font-bold text-xl md:text-2xl">
+                    {data?.data.title}
+                  </p>
+                  <p className="text-gray-700 md:text-lg">
+                    {data?.data.description}
+                  </p>
                 </div>
-                <div className="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 ">
-                  {" "}
-                  {""}
+                <div className="p-4 md:p-5 bg-gray-100 rounded-tl-xl">
+                  <div className="sm:flex sm:justify-between sm:items-center ">
+                    <div>
+                      <div className="text-lg text-gray-700">
+                        <span className="text-gray-900 font-bold">
+                          {data?.data.quantity}
+                        </span>{" "}
+                        Product
+                      </div>
+
+                      <div className="flex -mx-px">
+                        <Rating
+                          style={{ maxWidth: 100 }}
+                          value={data?.data.rating}
+                          readOnly
+                        />
+                      </div>
+                    </div>
+                    <div className="text-lg text-gray-700">
+                      <span className="text-gray-900 font-bold">Price:</span>{" "}
+                      {data?.data.price}
+                    </div>
+                    <button
+                      className="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 bg-blue-400 hover:bg-blue-500 font-bold text-white md:text-lg rounded-2xl shadow-md"
+                      onClick={() => handleCardDetails(data?.data)}
+                    >
+                      Add to Card
+                    </button>
+                  </div>
+                  <div className="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 ">
+                    {" "}
+                    {""}
+                  </div>
                 </div>
               </div>
             </div>
